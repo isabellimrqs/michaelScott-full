ebe4b4567519d88199c1457782aac599ae7dd454
ETS-EngineeringTechnicalSchool BOT-ResearchDevelopment (CaP/ETS) <ct67ca@bosch.com>
2024-05-02 14:03:23 -0300

    crud

--- api/main.py
+++ api/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from core.configs import settings
 from api.v1.api import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Api containing all the best sentences of Michael Scott")
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
